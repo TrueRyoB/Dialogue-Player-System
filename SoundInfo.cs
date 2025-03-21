89b31aa8058f1cc7e59160bf16544b5c6a763db7
TrueRyoB <121699111+TrueRyoB@users.noreply.github.com>
2025-03-21 17:31:50 -0500

    Update SoundInfo.cs

--- SoundInfo.cs
+++ SoundInfo.cs
@@ -8,7 +8,6 @@ namespace Fujin.Data
     public class SoundInfo : ScriptableObject
     {
         public string fileName = "*Insert a unique name here*";
-        // public string filePath;
         public ClipType clipType = ClipType.Oneshot;
         [Range(0.1f, 1f)] public float volumeScale = 1f;
         [Range(0.1f, 1f)] public float pitchScale = 1f;
